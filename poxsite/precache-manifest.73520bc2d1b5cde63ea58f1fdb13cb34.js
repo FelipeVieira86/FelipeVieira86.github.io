1cdd214c6159524ea38cfe07f2f9ef0dc760f6e4
Felipe Vieira <flpvieira86@gmail.com>
2020-07-17 14:04:37 -0300

    poxsite

---
+++ poxsite/precache-manifest.73520bc2d1b5cde63ea58f1fdb13cb34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e9a00b1ac57659ebc9b1cd666c2bb9e",
+    "url": "/poxsite/index.html"
+  },
+  {
+    "revision": "095c009f2f51199815fb",
+    "url": "/poxsite/static/css/2.d84ffcfc.chunk.css"
+  },
+  {
+    "revision": "d09122c8c29cf1b30155",
+    "url": "/poxsite/static/css/main.93573294.chunk.css"
+  },
+  {
+    "revision": "095c009f2f51199815fb",
+    "url": "/poxsite/static/js/2.95c0b27c.chunk.js"
+  },
+  {
+    "revision": "d0b30a1909a753c393e134caf98fa9bb",
+    "url": "/poxsite/static/js/2.95c0b27c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d09122c8c29cf1b30155",
+    "url": "/poxsite/static/js/main.7bac3ad1.chunk.js"
+  },
+  {
+    "revision": "7f4c25ee249670de41a0",
+    "url": "/poxsite/static/js/runtime-main.f6876b61.js"
+  }
+]);
